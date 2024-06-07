667beb4dc8ee0ef0242ebf7ad07ce35c24eff0ee
lxjonathanxl <cegavlog@gmail.com>
2024-06-07 18:05:48 -0300

    Add log capacity on wallet with docker writing on local log file
    what's changed:
    - Added logback.xml for configuration
    - Added a test log on WalletApplication

--- wallet/src/main/java/com/shares/wallet/WalletApplication.java
+++ wallet/src/main/java/com/shares/wallet/WalletApplication.java
@@ -1,13 +1,18 @@
 package com.shares.wallet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WalletApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(WalletApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(WalletApplication.class, args);
+		log.info("Application Started de boa");
 	}
 
 }
