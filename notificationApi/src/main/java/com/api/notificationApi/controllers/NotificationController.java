a78d5cd21680e800e9637ef9691056c26af65db3
lxjonathanxl <cegavlog@gmail.com>
2024-06-30 19:01:05 -0300

    Add capacity to send emails to notificationApi
    what's changed:
    - added EmailSenderService to send email
    - created postMapping "/sendEmail" to receive user transaction information and send email

--- notificationApi/src/main/java/com/api/notificationApi/controllers/NotificationController.java
+++ notificationApi/src/main/java/com/api/notificationApi/controllers/NotificationController.java
@@ -1,12 +1,14 @@
 package com.api.notificationApi.controllers;
 
+import com.api.notificationApi.service.EmailSenderService;
+import jakarta.mail.MessagingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
@@ -14,6 +16,12 @@ import java.util.Random;
 public class NotificationController {
 
     private static final Logger controllerLogger = LoggerFactory.getLogger(NotificationController.class);
+    private final EmailSenderService emailSenderService;
+
+    public NotificationController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
 
     @GetMapping("/get/result")
     public Boolean getResult() {
@@ -23,6 +31,22 @@ public class NotificationController {
         controllerLogger.info("api was called and will return: {}", result);
 
         return result;
+    }
+
+    @PostMapping("/sendEmail")
+    public String sendEmail(@RequestBody Map<String, String> request) throws MessagingException, UnsupportedEncodingException {
+        String email = request.get("email");
+
+        // Call the sendEmail method to send an email
+        String subject = "Hello from Spring Boot";
+        String content = "<p>Hello,</p><p>This is a test email sent from Spring Boot.</p>";
+
+        try {
+            emailSenderService.sendEmail(email, subject, content);
+            return "Email sent successfully.";
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            return "Failed to send email. Error: " + e.getMessage();
+        }
     }
 
 }
---
+++ notificationApi/src/main/java/com/api/notificationApi/service/EmailSenderService.java
@@ -0,0 +1,32 @@
+package com.api.notificationApi.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class EmailSenderService {
+
+    private JavaMailSender javaMailSender;
+
+    public EmailSenderService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String email, String subject, String content) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("stockwalletfinance@gmail.com", "Stock Wallet");
+        helper.setTo(email);
+
+        helper.setSubject(subject);
+        helper.setText(content, true);
+
+        javaMailSender.send(message);
+    }
+}
